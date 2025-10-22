99e3c425c175f6e5b56e38cd1e9e3bb0e2144140
Salas <salas@HDTUU.HOSPITAL>
2025-10-22 17:22:52 -0500

    Se sigue realizando el proyecto, hay errores que corregir

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,4 +1,47 @@
-export default function Page() {
-  return <p>Invoice Page</p>
-  
+import { Suspense } from 'react';
+import Search from '@/app/ui/search';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Table from '@/app/ui/invoices/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchFilteredInvoices } from '@/app/lib/data';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+
+async function InvoicesContent({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string; page?: string }>;
+}) {
+  const params = await searchParams;
+  const query = params?.query || '';
+  const currentPage = Number(params?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  const invoices = await fetchFilteredInvoices(query, currentPage);
+
+  return (
+    <>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Table query={query} currentPage={currentPage} />
+      <Pagination totalPages={totalPages} /> {/* Ajusta según tus datos */}
+    </>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string; page?: string }>;
+}) {
+  return (
+    <main>
+      <h1 className="text-2xl font-semibold">Invoices</h1>
+      <Suspense fallback={<p className="mt-6">Cargando facturas...</p>}>
+        <InvoicesContent searchParams={searchParams} />
+      </Suspense>
+    </main>
+  );
 }
